d40261c46a1d8f577cb6daf086e8a6f9e783634f
Teresa Nguyen <nguyentup@gmail.com>
2024-07-05 16:14:28 -0700

    Created restaurant table with dummy data

--- Restaurant Review React/src/App.tsx
+++ Restaurant Review React/src/App.tsx
@@ -2,13 +2,46 @@ import { useState } from "react";
 import Layout from "./components/Layout";
 import LoginForm from "./components/LoginForm";
 import RestaurantForm from "./components/RestaurantForm";
+import RestaurantList from "./components/RestaurantList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [restaurants, setRestaurants] = useState([
+    {
+      id: 1,
+      restaurantName: "aaa",
+      streetAddress: "123 sw h",
+      zipCode: "97225",
+      cuisine: "italian",
+      averageRating: 5,
+      comment: "good",
+    },
+    {
+      id: 2,
+      restaurantName: "bbb",
+      streetAddress: "123 sw h",
+      zipCode: "97225",
+      cuisine: "italian",
+      averageRating: 5,
+      comment: "good",
+    },
+    {
+      id: 3,
+      restaurantName: "ccc",
+      streetAddress: "123 sw h",
+      zipCode: "97225",
+      cuisine: "italian",
+      averageRating: 5,
+      comment: "good",
+    },
+  ]);
   return (
     <div>
-      <RestaurantForm />
+      <RestaurantList
+        restaurants={restaurants}
+        onDelete={(id) =>
+          setRestaurants(restaurants.filter((e) => e.id !== id))
+        }
+      />
     </div>
   );
 }
---
+++ Restaurant Review React/src/components/RestaurantList.tsx
@@ -0,0 +1,55 @@
+interface Restaurant {
+  id: number;
+  restaurantName: string;
+  streetAddress: string;
+  zipCode: string;
+  cuisine: string; // will eventually be an object, with id and name because of a drop down menu
+  averageRating: number; // will eventually be an object, with id and name because of a drop down menu
+  comment: string;
+}
+
+interface Props {
+  restaurants: Restaurant[];
+  onDelete: (id: number) => void;
+}
+
+const RestaurantList = ({ restaurants, onDelete }: Props) => {
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Restaurant Name</th>
+          <th>Street Address</th>
+          <th>Zip Code</th>
+          <th>Cuisine</th>
+          <th>Average Rating</th>
+          <th>Comment(s)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {restaurants.map((restaurant) => (
+          <tr key={restaurant.id}>
+            <td>{restaurant.restaurantName}</td>
+            <td>{restaurant.streetAddress}</td>
+            <td>{restaurant.zipCode}</td>
+            <td>{restaurant.cuisine}</td>
+            <td>{restaurant.averageRating}</td>
+            {/* Need to go back here to accumulate average rating */}
+            <td>{restaurant.comment}</td>
+            {/* Need to go back here to grab comments to put into a new page */}
+            <td>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => onDelete(restaurant.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default RestaurantList;

70d666cf4d3b07fee0191254bee3b47a1840fc27
Teresa Nguyen <nguyentup@gmail.com>
2024-07-11 16:18:43 -0700

    Added schema

--- Restaurant Review React/src/components/RestaurantForm.tsx
+++ Restaurant Review React/src/components/RestaurantForm.tsx
@@ -1,38 +1,88 @@
+import { date, z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ratings } from "../App";
 import { cuisines } from "../App";
 
+const schema = z.object({
+  restaurant: z.string(),
+  street: z.string(),
+  city: z.string(), // might need to change this if it's going to be a drop down menu?
+  zipcode: z.string(),
+  cuisine: z.enum(cuisines),
+  rating: z.number().optional(),
+  comment: z.string().max(300).optional().or(z.literal("")),
+});
+
+type RestaurantFormData = z.infer<typeof schema>;
+
 const RestaurantForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RestaurantFormData>({ resolver: zodResolver(schema) });
   return (
-    <form>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="restaurant" className="form-label">
           Restaurant Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          id="restaurant"
+          type="text"
+          className="form-control"
+          {...register("restaurant")}
+        />
+        {errors.restaurant && (
+          <p className="text-danger">{errors.restaurant.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="street" className="form-label">
           Address (Street Name)
         </label>
-        <input id="street" type="text" className="form-control" />
+        <input
+          id="street"
+          type="text"
+          className="form-control"
+          {...register("street")}
+        />
+        {errors.street && (
+          <p className="text-danger">{errors.street.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="city" className="form-label">
           City
         </label>
-        <input id="city" type="text" className="form-control" />
+        <input
+          id="city"
+          type="text"
+          className="form-control"
+          {...register("city")}
+        />
+        {errors.city && <p className="text-danger">{errors.city.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="zipcode" className="form-label">
           Zip Code
         </label>
-        <input id="zipcode" type="text" className="form-control" />
+        <input
+          id="zipcode"
+          type="text"
+          className="form-control"
+          {...register("zipcode")}
+        />
+        {errors.zipcode && (
+          <p className="text-danger">{errors.zipcode.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="cuisine" className="form-label">
           Cuisine Type
         </label>
-        <select id="cuisine" className="form-select">
+        <select {...register("cuisine")} id="cuisine" className="form-select">
           <option value=""></option>
           {cuisines.map((cuisine) => (
             <option key={cuisine} value={cuisine}>
@@ -40,12 +90,15 @@ const RestaurantForm = () => {
             </option>
           ))}
         </select>
+        {errors.cuisine && (
+          <p className="text-danger">{errors.cuisine.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="rating" className="form-label">
           Choose a Rating(1-5)
         </label>
-        <select id="rating" className="form-select">
+        <select {...register("rating")} id="rating" className="form-select">
           <option value=""></option>
           {ratings.map((rating) => (
             <option key={rating} value={rating}>
@@ -63,6 +116,7 @@ const RestaurantForm = () => {
       <div className="mb-3 input-group">
         <span className="input-group-text">Comment</span>
         <textarea
+          {...register("comment")}
           className="form-control"
           aria-label="Comment"
           id="comment"

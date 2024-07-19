f5fe63c7cda741af184c5ccb2b58d91a3b4dfa43
Teresa Nguyen <nguyentup@gmail.com>
2024-07-19 12:37:35 -0700

    imported bootstrap for modal

--- Restaurant Review React/src/main.tsx
+++ Restaurant Review React/src/main.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css";
 import Layout from "./components/Layout";
+import LoginForm from "./components/LoginForm";
+import RestaurantForm from "./components/RestaurantForm";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Layout>
       <App />
+      {/* <RestaurantForm onSubmit={data = }></RestaurantForm> */}
     </Layout>
   </React.StrictMode>
 );

88d564030cd1eea6327862c157bdebbf9bf49736
Teresa Nguyen <nguyentup@gmail.com>
2024-07-19 12:58:18 -0700

    Added modal login

--- Restaurant Review React/src/components/Layout.tsx
+++ Restaurant Review React/src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import LoginForm from "./LoginForm";
 
 interface Props {
   children: ReactNode;
@@ -10,7 +11,39 @@ function Layout({ children }: Props) {
       <div className="row">
         <header className="col-12 d-flex justify-content-between align-items-center py-3 mb-4 border-bottom">
           <h1 className="h3">Restaurant Review</h1>
-          <button className="btn btn-outline-primary">login</button>
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#loginModal"
+          >
+            Login
+          </button>
+
+          <div
+            className="modal fade"
+            id="loginModal"
+            aria-labelledby="loginModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <div className="mb-5">
+                    <LoginForm />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </header>
       </div>
       <div className="row">

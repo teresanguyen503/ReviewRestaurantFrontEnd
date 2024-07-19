399455e6316de78f8517abe58c591e5ebadaa39c
Teresa Nguyen <nguyentup@gmail.com>
2024-07-19 14:24:56 -0700

    Created file for security questions

---
+++ Restaurant Review React/src/securityQuestions.ts
@@ -0,0 +1,9 @@
+const securityQuestions = [
+    "What is your mother's maiden name?", 
+    "What street did you grow up on?", 
+    "What is your first pet's name?", 
+    "What high school did you go to?", 
+    "What is your favorite sports team?"
+] as const; 
+
+export default securityQuestions; 

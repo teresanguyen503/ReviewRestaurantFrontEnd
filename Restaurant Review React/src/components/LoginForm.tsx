f8e663314d327012dbf079096a6da0100056848f
Teresa Nguyen <nguyentup@gmail.com>
2024-08-29 10:30:31 -0700

    Updated login form, added schema

--- Restaurant Review React/src/components/LoginForm.tsx
+++ Restaurant Review React/src/components/LoginForm.tsx
@@ -1,17 +1,60 @@
-const LoginForm = () => {
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email required." })
+    .email("This is not a valid email."),
+  password: z.string().min(1, { message: "Password is required." }),
+});
+
+export type LoginFormData = z.infer<typeof schema>;
+
+interface Props {
+  onSubmit: (data: LoginFormData) => void;
+}
+
+const LoginForm = ({ onSubmit }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<LoginFormData>({ resolver: zodResolver(schema) });
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit((data) => {
+        onSubmit(data);
+        reset();
+      })}
+    >
       <div className="mb-3">
-        <label htmlFor="email" className="form-label">
+        <label htmlFor="email-login" className="form-label">
           Email
         </label>
-        <input id="email" type="email" className="form-control" />
+        <input
+          id="email-login"
+          type="email"
+          className="form-control"
+          {...register("email")}
+        />
+        {errors.email && <p className="text-danger">{errors.email.message}</p>}
       </div>
       <div className="mb-3">
-        <label htmlFor="password" className="form-label">
+        <label htmlFor="password-login" className="form-label">
           Password
         </label>
-        <input id="password" type="text" className="form-control" />
+        <input
+          id="password-login"
+          type="text"
+          className="form-control"
+          {...register("password")}
+        />
+        {errors.password && (
+          <p className="text-danger">{errors.password.message}</p>
+        )}
       </div>
       <button className="btn btn-primary" type="submit">
         Login
